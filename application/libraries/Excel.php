19ae9155d549e90d10d6acb6ce027e9a891812ff
adji142 <prasetyoajiw@gmail.com>
2023-07-11 15:39:25 +0700

    add library

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -1,7 +1,7 @@
 <?php 
 if (!defined('BASEPATH')) exit('No direct script access allowed');  
   
-require_once APPPATH."/third_party/PHPExcel.php";
+require_once('PHPExcel.php');
   
 class Excel extends PHPExcel {
     public function __construct() {
