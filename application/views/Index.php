b6f6749e4e6555951a50dedd6873e0a5c2fe9cc3
adji142 <prasetyoajiw@gmail.com>
2024-08-01 13:18:04 +0700

    Update Index.php

--- application/views/Index.php
+++ application/views/Index.php
@@ -18,7 +18,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>AIS SYSTEM! | </title>
+    <title>SMK AL Islam Kalijambe</title>
 
     <!-- Bootstrap -->
     <link href="<?php echo base_url();?>Assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
